3a041560a16b6da1c3a96a78e1ef4e09332583e2
= <=>
2023-03-03 13:00:02 -0500

    Finished WhoWroteIt App

--- WhoWroteIt/app/src/main/java/com/example/whowroteit/FetchBook.java
+++ WhoWroteIt/app/src/main/java/com/example/whowroteit/FetchBook.java
@@ -3,6 +3,10 @@ package com.example.whowroteit;
 import android.os.AsyncTask;
 import android.widget.TextView;
 
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
 import java.lang.ref.WeakReference;
 
 public class FetchBook extends AsyncTask<String, Void, String> {
@@ -22,5 +26,40 @@ public class FetchBook extends AsyncTask<String, Void, String> {
     @Override
     protected void onPostExecute(String s) {
         super.onPostExecute(s);
+
+        try {
+            JSONObject jsonObject = new JSONObject(s);
+            JSONArray itemsArray = jsonObject.getJSONArray("items");
+
+            int i = 0;
+            String title = null;
+            String authors = null;
+
+            while (i < itemsArray.length() &&
+                    (authors == null && title == null)) {
+                JSONObject book = itemsArray.getJSONObject(i);
+                JSONObject volumeInfo = book.getJSONObject("volumeInfo");
+
+                try {
+                    title = volumeInfo.getString("title");
+                    authors = volumeInfo.getString("authors");
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
+                i++;
+            }
+            if (title != null && authors != null) {
+                mTitleText.get().setText(title);
+                mAuthorText.get().setText(authors);
+            }
+            else {
+                mTitleText.get().setText(R.string.no_results_text);
+                mAuthorText.get().setText("");
+            }
+        } catch (Exception e) {
+            mTitleText.get().setText(R.string.no_results_text);
+            mAuthorText.get().setText("");
+            e.printStackTrace();
+        }
     }
 }
